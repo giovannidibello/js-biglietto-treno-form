a73d4306009239266d544eec53d63262a8832536
Giovanni <dibellogio@gmail.com>
2025-01-13 15:49:15 +0100

    add js output milestone 2

--- js/script.js
+++ js/script.js
@@ -5,6 +5,10 @@ const Km = document.getElementById("campoNumeroKm");
 const eta = document.getElementById("fasciaEta");
 const bottone = document.querySelector("button");
 
+// seleziono i dati in output
+const offerta = document.getElementById("offerta");
+const prezzoBiglietto = document.getElementById("prezzo");
+
 // ELABORAZIONE
 
 // genero il biglietto al click sul pulsante
@@ -23,17 +27,24 @@ bottone.addEventListener("click", function (event) {
         // applica uno sconto del 20%
         prezzo *= 0.80;
         console.log("Hai avuto uno sconto del 20%. Prezzo finale: €" + prezzo.toFixed(2));
+        offerta.innerText = "Biglietto Scontato del 20%"
+        prezzoBiglietto.innerText = prezzo.toFixed(2)+"€";
+
 
         // SE l'utente è over 65     
     } else if (fasciaEta === "over65") {
         // applica uno sconto del 40%
         prezzo *= 0.60;
         console.log("Hai avuto uno sconto del 40%. Prezzo finale: €" + prezzo.toFixed(2));
+        offerta.innerText = "Biglietto Scontato del 40%"
+        prezzoBiglietto.innerText = prezzo.toFixed(2)+"€";
 
         // altrimenti
     } else {
         // nessuno sconto
         console.log("Non hai diritto a nessun sconto. Prezzo finale: €" + prezzo.toFixed(2));
+        offerta.innerText = "Biglietto Standard"
+        prezzoBiglietto.innerText = prezzo.toFixed(2)+"€";
     }
 
 });
