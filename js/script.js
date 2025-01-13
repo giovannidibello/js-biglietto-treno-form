347acec4544a9fcbecbd9bb84190e39a9a78dacc
Giovanni <dibellogio@gmail.com>
2025-01-13 15:30:51 +0100

    add js form milestone 1

---
+++ js/script.js
@@ -0,0 +1,40 @@
+// INPUT
+
+// seleziono i dati inseriti dall'utente
+const Km = document.getElementById("campoNumeroKm");
+const eta = document.getElementById("fasciaEta");
+const bottone = document.querySelector("button");
+
+// ELABORAZIONE
+
+// genero il biglietto al click sul pulsante
+bottone.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    // ottengo i valori inseriti dall'utente
+    const numerokm = parseFloat(Km.value);
+    const fasciaEta = eta.value;
+
+    // definizione prezzo base del biglietto
+    let prezzo = numerokm * 0.21;
+
+    // SE l'utente è minorenne 
+    if (fasciaEta === "minorenne") {
+        // applica uno sconto del 20%
+        prezzo *= 0.80;
+        console.log("Hai avuto uno sconto del 20%. Prezzo finale: €" + prezzo.toFixed(2));
+
+        // SE l'utente è over 65     
+    } else if (fasciaEta === "over65") {
+        // applica uno sconto del 40%
+        prezzo *= 0.60;
+        console.log("Hai avuto uno sconto del 40%. Prezzo finale: €" + prezzo.toFixed(2));
+
+        // altrimenti
+    } else {
+        // nessuno sconto
+        console.log("Non hai diritto a nessun sconto. Prezzo finale: €" + prezzo.toFixed(2));
+    }
+
+});
+
